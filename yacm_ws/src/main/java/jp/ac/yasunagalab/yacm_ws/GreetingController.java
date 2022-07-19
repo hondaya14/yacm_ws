5ffa4a296168ec78591cc3433753442ed208035c
Yasuhisa Honda <nqvno14@gmail.com>
2022-07-19 19:48:19 +0900

    Implement simple form

--- yacm_ws/src/main/java/jp/ac/yasunagalab/yacm_ws/GreetingController.java
+++ yacm_ws/src/main/java/jp/ac/yasunagalab/yacm_ws/GreetingController.java
@@ -4,9 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GreetingController {
@@ -18,9 +16,21 @@ public class GreetingController {
 //        return new Greeting(counter.incrementAndGet(), String.format(template, name));
 //    }
 
+//    @GetMapping("/greeting")
+//    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "greeting";
+//    }
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
         return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
     }
 }
